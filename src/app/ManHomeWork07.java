7e1f500d0bd07422088955174bc747404cc2b0a2
anna-masl <anna.maslinnikova@gmail.com>
2024-10-08 23:06:52 +0300

    second

--- src/app/ManHomeWork07.java
+++ src/app/ManHomeWork07.java
@@ -15,38 +15,43 @@ public class ManHomeWork07 {
 
         for (int index = 0; index < array.length; index++) {
             array[index] = random.nextInt(201) - 100;
-
             if (array[index] < 0) {
                 sum += array[index];
             }
+        }
+        int maxvalue = array[0];
+        int minvalue = maxvalue;
+        int keymaxvalue = 0;
+        int keyminvalue = 0;
+
+        System.out.println(Arrays.toString(array));
+        System.out.println("Сума від'ємних чисел: " + sum);
+
+        for (int index = 0; index < array.length; index++) {
+
             if (array[index] % 2 == 0) {
                 counteven++;
             } else countodd++;
+            if (array[index] > maxvalue) {
+                maxvalue = array[index];
+                keymaxvalue = index;
+            } else if (array[index] < minvalue) {
+                minvalue = array[index];
+                keyminvalue = index;
+            }
         }
 
-        int[] arraycopy = Arrays.copyOf(array, array.length);
-        Arrays.sort(arraycopy);
-        int minvalue = arraycopy[0];
-        int maxvalue = arraycopy[arraycopy.length - 1];
-        int keymin = Arrays.binarySearch(array, minvalue);
-        System.out.println(keymin);
-        int keymax = Arrays.binarySearch(array, maxvalue);
-        System.out.println(keymax);
-
-        System.out.println(Arrays.toString(array));
-        System.out.println(Arrays.toString(arraycopy));
-        System.out.println("Сума від'ємних чисел: " + sum);
         System.out.println("Кількість парних чисел: " + counteven);
         System.out.println("Кількість непарних чисел: " + countodd);
-        System.out.println("Найменший елемент: " + minvalue + " (з індексом " + keymin + ")");
-        System.out.println("Найбільший елемент: " + maxvalue + " (з індексом " + keymax + ")");
-        for (int i = 0; i <array.length; i++) {
-            if (array[i] < 0){
-                System.out.println(i);
-                break;
-            } else System.out.println("Від'ємних чисел немає");
-            break;
+        System.out.println("Найменший елемент: " + minvalue + " (з індексом " + keyminvalue + ")");
+        System.out.println("Найбільший елемент: " + maxvalue + " (з індексом " + keymaxvalue + ")");
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] < 0) {
+//                System.out.println(i);
+//                break;
+//            } else System.out.println("Від'ємних чисел немає");
+//            break;
 
-        }
     }
 }
+
